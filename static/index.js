5b48bb4684915fefc2684b94981859246a46a0d8
MAD <madgroupware@gmail.com>
2012-06-30 21:38:38 +0530

    fourth commit

---
+++ static/index.js
@@ -0,0 +1,9 @@
+function watermark(inputId,text){
+  var inputBox = document.getElementById(inputId);
+    if (inputBox.value.length > 0){
+      if (inputBox.value == text)
+        inputBox.value = '';
+    }
+    else
+      inputBox.value = text;
+}
